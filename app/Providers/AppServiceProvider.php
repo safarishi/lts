5ded0d660e0b778048de5278c2ca693e4b5bcab4
safari <sshsfl@yeah.net>
2015-10-29 15:58:28 +0800

    exception error todo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+// use Illuminate\Http\JsonResponse;
 use Illuminate\Support\ServiceProvider;
+// use App\Exceptions\Api\ApiException;
+// use App\Exceptions\Api\ValidationException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +27,36 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        // $this->registerExceptionHandlers();
     }
+
+    /**
+     * Retister exception handlers
+     *
+     * @return mixed
+     */
+    // private function registerExceptionHandlers()
+    // {
+    //     $this->app->call(function(ApiException $e) {
+    //         return $this->handleException($e);
+    //     });
+    // }
+
+    // private function handleException($exception)
+    // {
+    //     $res = [
+    //         'error' => $exception->errorType,
+    //         'error_description' => $exception->getMessage(),
+    //     ];
+
+    //     if ($code = $exception->getCode()) {
+    //         $res['error_code'] = $code;
+    //     }
+
+    //     if ($uri = $exception->errorUri) {
+    //         $res['error_uri'] = $uri;
+    //     }
+
+    //     return new JsonResponse($res, $exception->httpStatusCode, $exception->getHttpHeaders());
+    // }
 }
