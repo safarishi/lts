5fa641c140ca6ef43937378f9d911ba339f46868
safari <sshsfl@yeah.net>
2015-11-24 10:23:56 +0800

    add comment for config oauth2.php

--- config/oauth2.php
+++ config/oauth2.php
@@ -11,6 +11,16 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connection to use
+    |--------------------------------------------------------------------------
+    |
+    | Set the default database connection to use for the repositories,
+    | when set to default, it uses whatever connection you specified in your laravel db config.
+    |
+    */
+
     'database' => 'mysql',
 
     /*
