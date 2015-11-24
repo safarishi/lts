30a93485ba6230a0ef0704751955ee604b7b6d76
safari <sshsfl@yeah.net>
2015-11-24 10:52:12 +0800

    修改 route 增加namespace

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,51 +19,51 @@ Route::get('/', function()
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', function($api)
-{
-    $api->get('articles', 'App\Http\Controllers\ArticleController@index');
-    $api->get('articles/{id}', 'App\Http\Controllers\ArticleController@show');
-    $api->get('reports', 'App\Http\Controllers\ArticleController@report');
-    $api->put('articles/{id}/stars', 'App\Http\Controllers\ArticleController@star');
-    $api->delete('articles/{id}/stars', 'App\Http\Controllers\ArticleController@unstar');
-    $api->post('articles/{id}/comments', 'App\Http\Controllers\ArticleController@comment');
-    $api->post('articles/{id}/anonymous_comments', 'App\Http\Controllers\ArticleController@anonymousComment');
+$api->version('v1', ['namespace' => 'App\Http\Controllers'], function ($api) {
+    // app route list
+    $api->get('articles', 'ArticleController@index');
+    $api->get('articles/{id}', 'ArticleController@show');
+    $api->get('reports', 'ArticleController@report');
+    $api->put('articles/{id}/stars', 'ArticleController@star');
+    $api->delete('articles/{id}/stars', 'ArticleController@unstar');
+    $api->post('articles/{id}/comments', 'ArticleController@comment');
+    $api->post('articles/{id}/anonymous_comments', 'ArticleController@anonymousComment');
     // 用户注册
-    $api->post('users', 'App\Http\Controllers\UserController@store');
+    $api->post('users', 'UserController@store');
     // 用户登录
-    $api->post('oauth/access_token', 'App\Http\Controllers\OauthController@postAccessToken');
+    $api->post('oauth/access_token', 'OauthController@postAccessToken');
     // 退出登录
-    $api->delete('oauth/invalidate_token', 'App\Http\Controllers\UserController@logout');
-    $api->post('articles/{id}/comments/{comment_id}/replies', 'App\Http\Controllers\ArticleController@reply');
-    $api->post('articles/{id}/comments/{comment_id}/anonymous_replies', 'App\Http\Controllers\ArticleController@anonymousReply');
-    $api->put('articles/{id}/comments/{comment_id}/favours', 'App\Http\Controllers\ArticleController@favour');
-    $api->delete('articles/{id}/comments/{comment_id}/favours', 'App\Http\Controllers\ArticleController@unfavour');
-    $api->get('articles/{id}/comments', 'App\Http\Controllers\ArticleController@commentList');
-    $api->get('search/articles', 'App\Http\Controllers\ArticleController@search');
-    $api->get('more_articles/{column_id}', 'App\Http\Controllers\ArticleController@moreArticle');
-    $api->get('user', 'App\Http\Controllers\UserController@show');
-    $api->get('user/comments', 'App\Http\Controllers\UserController@myComment');
-    $api->get('user/stars', 'App\Http\Controllers\UserController@myStar');
-    $api->get('user/informations', 'App\Http\Controllers\UserController@myInformation');
+    $api->delete('oauth/invalidate_token', 'UserController@logout');
+    $api->post('articles/{id}/comments/{comment_id}/replies', 'ArticleController@reply');
+    $api->post('articles/{id}/comments/{comment_id}/anonymous_replies', 'ArticleController@anonymousReply');
+    $api->put('articles/{id}/comments/{comment_id}/favours', 'ArticleController@favour');
+    $api->delete('articles/{id}/comments/{comment_id}/favours', 'ArticleController@unfavour');
+    $api->get('articles/{id}/comments', 'ArticleController@commentList');
+    $api->get('search/articles', 'ArticleController@search');
+    $api->get('more_articles/{column_id}', 'ArticleController@moreArticle');
+    $api->get('user', 'UserController@show');
+    $api->get('user/comments', 'UserController@myComment');
+    $api->get('user/stars', 'UserController@myStar');
+    $api->get('user/informations', 'UserController@myInformation');
     // 修改用户个人信息
-    $api->post('user', 'App\Http\Controllers\UserController@modify');
-    $api->get('products', 'App\Http\Controllers\ArticleController@product');
-    $api->get('teams', 'App\Http\Controllers\ArticleController@team');
+    $api->post('user', 'UserController@modify');
+    $api->get('products', 'ArticleController@product');
+    $api->get('teams', 'ArticleController@team');
     // generate token
-    $api->get('generate_token', 'App\Http\Controllers\MultiplexController@generateToken');
-    $api->get('generate_captcha', 'App\Http\Controllers\MultiplexController@generateCaptcha');
+    $api->get('generate_token', 'MultiplexController@generateToken');
+    $api->get('generate_captcha', 'MultiplexController@generateCaptcha');
     // third party login url version 1
-    $api->get('weibo_url', 'App\Http\Controllers\ThirdPartyLoginController@generateWeiboUrl');
-    $api->get('qq_url', 'App\Http\Controllers\ThirdPartyLoginController@generateQqUrl');
-    $api->get('weixin_url', 'App\Http\Controllers\ThirdPartyLoginController@generateWeixinUrl');
+    $api->get('weibo_url', 'ThirdPartyLoginController@generateWeiboUrl');
+    $api->get('qq_url', 'ThirdPartyLoginController@generateQqUrl');
+    $api->get('weixin_url', 'ThirdPartyLoginController@generateWeixinUrl');
     // third party login url version 2
-    $api->get('redirect_url/{type}', 'App\Http\Controllers\ThirdPartyLoginController@redirectUrl');
-    $api->get('weibo_callback', 'App\Http\Controllers\ThirdPartyLoginController@weiboCallback');
-    $api->get('entry', 'App\Http\Controllers\ThirdPartyLoginController@entry');
+    $api->get('redirect_url/{type}', 'ThirdPartyLoginController@redirectUrl');
+    $api->get('weibo_callback', 'ThirdPartyLoginController@weiboCallback');
+    $api->get('entry', 'ThirdPartyLoginController@entry');
     // 小红点
-    $api->get('user/notices', 'App\Http\Controllers\UserController@notice');
+    $api->get('user/notices', 'UserController@notice');
     // 点击红点
-    $api->delete('user/notices', 'App\Http\Controllers\UserController@removeNotice');
+    $api->delete('user/notices', 'UserController@removeNotice');
 });
 
 Route::group(array('prefix' => 'v2'), function()
