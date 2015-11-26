814b69e08444001873ccfaea8be86e07786f2d1f
safari <sshsfl@yeah.net>
2015-11-26 23:50:42 +0800

    增加 oauth2 refresh token

--- config/oauth2.php
+++ config/oauth2.php
@@ -45,11 +45,11 @@ return [
             'callback' => '\App\PasswordVerifier@verify',
             'access_token_ttl' => 3600
         ],
-        // 'refresh_token' => [
-        //     'class' => '\League\OAuth2\Server\Grant\RefreshTokenGrant',
-        //     'access_token_ttl' => 3600,
-        //     'refresh_token_ttl' => 36000
-        // ],
+        'refresh_token' => [
+            'class' => '\League\OAuth2\Server\Grant\RefreshTokenGrant',
+            'access_token_ttl' => 3600,
+            'refresh_token_ttl' => 36000
+        ],
 
         'client_credentials' => [
             'class' => '\League\OAuth2\Server\Grant\ClientCredentialsGrant',
