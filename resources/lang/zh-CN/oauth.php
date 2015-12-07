6d78e57083fff654e8b835b2ea843caa9af8e205
safari <sshsfl@yeah.net>
2015-12-07 14:58:18 +0800

    增加 oauth2 错误提示消息

---
+++ resources/lang/zh-CN/oauth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'invalid_credentials'    => '帐号密码不正确。',
+    'access_denied'          => '验证服务器拒绝访问。',
+    'invalid_client'         => '客户端验证失败。',
+    'invalid_grant'          => '无效的授权方式。',
+    'unsupported_grant_type' => '不支持的授权方式。',
+    'unauthorized_client'    => '未授权的客户端。',
+    'server_error'           => '服务器报告了一个错误。',
+    'invalid_refresh_token'  => '无效的刷新令牌。',
+    'invalid_request'        => '无效的请求，请检查 :parameter 参数。',
+    // todo
+    // 'invalid_scope' => '', // scope mode
+    // 'unsupported_response_type' => '', // AuthCodeGrant
+
+];
