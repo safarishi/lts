555929fe1b28667ad464590e368912ffefb3b4bf
safari <sshsfl@yeah.net>
2015-12-08 19:49:45 +0800

    增加文章客户端校验

--- app/Http/Controllers/ArticleV1Controller.php
+++ app/Http/Controllers/ArticleV1Controller.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\ValidationException;
+use LucaDegasperi\OAuth2Server\Authorizer;
 
 class ArticleV1Controller extends ArticleController
 {
+    public function __construct(Authorizer $authorizer)
+    {
+        parent::__construct($authorizer);
+        $this->middleware('validation');
+    }
+
+    private static $_validate = [
+        'comment' => [
+            'content' => 'required',
+        ],
+        'anonymousComment' => [
+            'content' => 'required',
+        ],
+    ];
+
     public function show($id)
     {
         $article = $this->getArticleById($id);
