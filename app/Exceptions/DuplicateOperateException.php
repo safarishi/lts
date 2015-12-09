77aa93f58e1e3f91827381980ac8cd528e87de34
safari <sshsfl@yeah.net>
2015-12-09 11:11:11 +0800

    修改 异常类：重复操作 修改http status code 409 conflit 修改 错误类型

--- app/Exceptions/DuplicateOperateException.php
+++ app/Exceptions/DuplicateOperateException.php
@@ -7,7 +7,7 @@ class DuplicateOperateException extends ApiException
     public function __construct($msg)
     {
         parent::__construct($msg, 14003);
-        $this->httpStatusCode = 400;
-        $this->errorType = 'invalid_operation';
+        $this->httpStatusCode = 409;
+        $this->errorType = 'conflict_operation';
     }
 }
